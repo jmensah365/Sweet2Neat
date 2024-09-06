006f443cb0ca2bfb9bd12af51c448317e85ebb96
d-swe <djkim.swe@gmail.com>
2024-09-06 13:50:10 -0400

    add all feature files back

--- Testing/project2-testing/src/test/java/com/skillstorm/RunCucumberTest.java
+++ Testing/project2-testing/src/test/java/com/skillstorm/RunCucumberTest.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.CucumberOptions;
 
 // cucumberoptions used for testng
 @CucumberOptions(
-    features = "classpath:com/skillstorm/candy.feature", 
+    features = "classpath:com/skillstorm/", 
     glue = "com.skillstorm",
     plugin = {"pretty", "html:target/cucumber-reports.html"} 
 )
